650caab0421305898e1b4e319889478fb77f40b8
Richard Nienhuis <richardnienhuis@gmail.com>
2017-12-29 16:51:43 -0500

    Add error to search by code if not found

--- redproject/views/index.py
+++ redproject/views/index.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from ..forms import ParticipantForm
+from ..models import Participant
 
 blueprint = Blueprint('index', __name__)
 
@@ -10,15 +11,24 @@ blueprint = Blueprint('index', __name__)
 @blueprint.route("/participant_search/",methods=['GET','POST'])
 @login_required
 def index():
+    error = None
     form=ParticipantForm(request.form)
+    print(form.data)
+    print(form.validate_on_submit())
     if form.validate_on_submit():
+        print('post val')
         if form.find_by_code.data:
             """check for participant code in db"""
-            print(url_for('index.existing_client',participant_code=form.participant_code.data))
-            return redirect(url_for('index.existing_client',participant_code=form.participant_code.data))
+            participant = Participant.objects(participant_code=form.participant_code.data).first()
+            print(participant)
+            if participant:
+                return redirect(url_for('index.existing_client',participant_code=form.participant_code.data))
+            else:
+                error = 'Invalid Participant'
         else:
             """check for participant by parts and goto next step if doesn't exist"""
-    return render_template("index.html",form=form)
+        print(error)
+    return render_template("index.html",form=form,error=error)
 
 @blueprint.route("/existing_client/",methods=['GET','POST'])
 @login_required
